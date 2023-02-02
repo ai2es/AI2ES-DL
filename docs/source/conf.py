15e9a229d4a0b6bd3432c4fd95b869cb2cdd4e0b
JayRoth-sch <35668950+JayRothenberger@users.noreply.github.com>
2023-02-02 14:30:09 -0600

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,7 +34,7 @@ extensions = [
     'sphinx.ext.duration',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
-    'python-docs-theme',
+    'python_docs_theme',
 ]
 autosummary_generate = True
 
@@ -52,7 +52,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'python-docs-theme'
+html_theme = 'python_docs_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
