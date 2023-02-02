eafc564487018a9a0493dbdd15f339cc5c9707a0
JayRoth-sch <35668950+JayRothenberger@users.noreply.github.com>
2023-02-02 14:40:07 -0600

    theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,6 +12,7 @@
 #
 import os
 import sys
+
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.append(os.path.abspath('../..'))
 sys.path.append(os.path.abspath('..'))
@@ -34,7 +35,6 @@ extensions = [
     'sphinx.ext.duration',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
-    'python_docs_theme',
 ]
 autosummary_generate = True
 
@@ -52,7 +52,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'python_docs_theme'
+html_theme = 'renku'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
